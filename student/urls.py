2e0dab737c58d822a12539f6745251da9e4c6136
edmar.james <edmarjamesbautista@gmail.com>
2024-10-08 19:27:57 +0800

    Add new function for Union

--- student/urls.py
+++ student/urls.py
@@ -5,5 +5,6 @@ app_name = 'student'
 
 urlpatterns = [
     path('', views.student_list, name='student_data'),
-    path('and/', views.student_list_and, name='student_data_and')
+    path('and/', views.student_list_and, name='student_data_and'),
+    path('union/', views.student_list_union, name='student_data_union')
 ]
--- student/views.py
+++ student/views.py
@@ -1,7 +1,11 @@
-from .models import Student
+from .models import (
+    Student,
+    Teacher
+)
 from django.db import connection
 from django.db.models import Q
 from django.shortcuts import render
+
 
 # Part 2
 #################################################################
@@ -18,27 +22,28 @@ def student_list_(request):
     print(">>> Execution details")
     print(connection.queries)
 
-    return render(request, 'output.html', {'posts':posts})
+    return render(request, "output.html", {"posts":posts})
 
 def student_list(request):
     # This query use the "|" symbol as "OR"
     # posts = Student.objects.filter(surname__startswith='Bautista') | Student.objects.filter(surname__startswith='Doe')
 
     # It can be simplified using "Q" objects together with the "|" symbol as OR
-    posts = Student.objects.filter(Q(surname__startswith='Bautista') | Q(surname__startswith='Doe'))
+    posts = Student.objects.filter(Q(surname__startswith="Bautista") | Q(surname__startswith="Doe"))
 
     # Use can use tilde symbol "~" as not together with the "Q" object
-    not_posts = Student.objects.filter(Q(surname__startswith='Bautista') | ~Q(surname__startswith='Doe'))
-
-    context = {
-        'posts':posts,
-        'not_posts': not_posts
-    }
+    not_posts = Student.objects.filter(Q(surname__startswith="Bautista") | ~Q(surname__startswith="Doe"))
 
     print(posts)
     print(connection.queries)
 
-    return render(request, 'output.html', context)
+    context = {
+        "posts": posts,
+        "not_posts": not_posts
+    }
+
+    return render(request, "output.html", context)
+
 
 # Part 3
 #################################################################
@@ -47,13 +52,30 @@ def student_list_and(request):
     # and_post = Student.objects.filter(classroom=3) & Student.objects.filter(firstname__startswith='Jane')
 
     # It can be simplified using "Q" objects together with the "&" symbol as AND
-    and_post = Student.objects.filter(Q(classroom=3) & Q(firstname__startswith='Jane'))
-
-    context = {
-        'and_post':and_post
-    }
+    and_post = Student.objects.filter(Q(classroom=3) & Q(firstname__startswith="Jane"))
 
     print(and_post)
     print(connection.queries)
 
-    return render(request, 'output.html', context)
+    context = {
+        "and_post": and_post
+    }
+
+    return render(request, "output.html", context)
+
+
+# Part 4
+#################################################################
+def student_list_union(request):
+    # Union is used to combine the results of two select query
+    # Union remove duplicates from the two combined query
+    union_post = Student.objects.all().values_list("firstname").union(Teacher.objects.all().values_list("firstname"))
+
+    print(union_post)
+    print(connection.queries)
+
+    context = {
+        "union_post": union_post
+    }
+
+    return render(request, "output.html", context)
