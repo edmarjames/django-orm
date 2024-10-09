38349aee10388e88f59dcd9a6603845973e06cb2
edmar.james <edmarjamesbautista@gmail.com>
2024-10-09 17:20:51 +0800

    Create new models for Model inheritance options

--- student/models.py
+++ student/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Model Tasks 1-5
 #####################################
@@ -23,3 +24,61 @@ class Student(models.Model):
         return self.firstname
 
 ######################################
+
+# Proxy models
+# Change the behavior of a model
+# Proxy models operate on the original model
+class BookContent(models.Model):
+    title = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+class BookOrders(BookContent):
+    class Meta:
+        proxy = True
+        ordering = ["created"]
+
+    def created_on(self):
+        return timezone.now() - self.created
+
+
+# Multi table model inheritance
+# Every model is a model all by itself
+# One to one link is created automatically
+class Books(models.Model):
+    title = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+class ISBN(Books):
+    books_ptr = models.OneToOneField(
+        Books, on_delete=models.CASCADE,
+        parent_link=True,
+        primary_key=True,
+    )
+    ISBN = models.TextField()
+
+
+# Abstract Model
+# Used when you have common information needed for number of other models
+class BaseItem(models.Model):
+    title = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ["title"]
+
+class ItemA(BaseItem):
+    content = models.TextField()
+
+    class Meta(BaseItem.Meta):
+        ordering = ["-created"]
+
+class ItemB(BaseItem):
+    file = models.FileField(upload_to="files")
+
+class ItemC(BaseItem):
+    file = models.FileField(upload_to="images")
+
+class ItemD(BaseItem):
+    slug = models.SlugField(max_length=255, unique=True)
